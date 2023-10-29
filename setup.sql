6c7ec2221251fefd4b989ed67b94b4a07145620c
cconyers164 <chrconyers@gmail.com>
2023-10-29 10:45:38 -0400

    Updated setup.sql

--- setup.sql
+++ setup.sql
@@ -1,17 +1,22 @@
+DROP DATABASE if EXISTS passwords;
 CREATE DATABASE passwords;
+SET block_encryption_mode = 'aes-256-cbc';
+SET @init_vector = RANDOM_BYTES(16);
+
 USE passwords; 
 
-CREATE TABLE user(
-    id INT AUTO_INCREMENT,
-    first_name varchar(50),
-    last_name varchar(50),
+CREATE TABLE user_info(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    website_name varchar(255)
+    website_URL TEXT DEFAULT NULL,
+    f_name varchar(50),
+    l_name varchar(50),
     username varchar(50),
     email_address varchar(255),
     pwd BLOB NOT NULL,
-    comment TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    website_URL TEXT DEFAULT NULL,
-    PRIMARY KEY (id)
+    comment TEXT,
+
 );
 
     
